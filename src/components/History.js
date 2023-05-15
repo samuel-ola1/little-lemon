98e8a9a82daa0c1372a44c68b6bdeac83108770e
Samuel Olayande <samuelolayande@gmail.com>
2023-05-15 22:48:29 +0100

    worked on history section

--- src/components/History.js
+++ src/components/History.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import mario from "./assets/Mario and Adrian A.jpg";
 import adrian from "./assets/Mario and Adrian b.jpg";
+import "./history.css"
 
 
 
 const History = () => {
   return (
-    <section>
-        <div className="container">
-            <div>
+    <section className="history">
+        <div className="container grid">
+            <div className="history-text">
                 <h2 className="title">Little Lemon</h2>
                 <h3>Chicago</h3>
                 <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. At voluptatem pariatur enim cupiditate, odit quisquam dolorum delectus fuga impedit obcaecati voluptates soluta modi beatae facere tenetur perferendis consectetur numquam nulla ea, reiciendis aut unde doloribus eum neque! Dicta, autem nostrum!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, delectus repellat hic excepturi illo vitae nemo quas? Beatae consequatur delectus ut magni ad doloremque nisi, fuga culpa est officiis doloribus blanditiis voluptatibus nostrum ipsa accusamus amet corporis facere! Tenetur iure aperiam porro! Obcaecati neque voluptatem quisquam itaque necessitatibus vel quasi, tempora adipisci architecto at. Voluptas ullam cupiditate accusamus explicabo nihil esse dolorem odio ipsa, quisquam est voluptatum in quas nam, ratione, minima omnis fuga. Commodi illo aliquid nisi corporis harum.
                 </p>
             </div>
-            <div>
+            <div className='history-images'>
                 <img src={mario} alt="" />
                 <img src={adrian} alt="" />
             </div>
