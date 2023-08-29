5e68385be6dbb1923ded14d33e90a9186100c442
Olayande Samuel <samuelolayande@gmail.com>
2023-08-29 15:52:50 +0100

    styling

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,32 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from 'yup';
+
+const schema = yup.object({
+  name: yup.string().required("Enter you name"),
+  email: yup.string().required("Enter your email address"),
+  telephone: yup.string().required("Enter your phone number").matches(/^\(\d{3}\) \d{3}-\d{4}$/, "Phone Number must match (123) 456-7890 format"),
+  guests: yup.number().min(1, "There must be at least one guest!").required("Please specify the number of guests, per table"),
+  date: yup.string().required("Enter the date and time")
+})
+
+
 
 const BookingForm = () => {
 
-
-
+  const { handleSubmit, register, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  })
+  
+  const onSubmit = (data) => {
+    console.log(data);
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
         <fieldset>
           <div className="field">
             <label htmlFor="name">Full Name</label>
-            <input type="text" placeholder="Joe Biden" name="name"/>
-            <span>Errors</span>
+            <input type="text" placeholder="Joe Biden" name="name" {...register("name")}/>
+            <span>{errors.name?.message}</span>
           </div>
           <div className="field">
             <label htmlFor="email">Email Address</label>
-            <input type="email" placeholder="joebiden@email.com" name="email"/>
-            <span>Errors</span>
+            <input type="email" placeholder="joebiden@email.com" name="email" {...register("email")}/>
+            <span>{errors.email?.message}</span>
           </div>
           <div className="field">
             <label htmlFor="telephone">Telephone Number</label>
-            <input type="tel" placeholder="(123) 456-7890" name="telephone"/>
-            <span>Errors</span>
+            <input type="tel" placeholder="(123) 456-7890" name="telephone" {...register("telephone")}/>
+            <span>{errors.telephone?.message}</span>
           </div>
 
           <div className='field'>
               <label htmlFor="occasion" className="htmlfor">Occasion</label>
-              <select name="occasion" className="name">
+              <select name="occasion" className="name" {...register("occasion")}>
                 <option value="select">Select Occasion</option>
                 <option value="birthday">Birthday</option>
                 <option value="engagement">Engagement</option>
@@ -36,16 +54,16 @@ const BookingForm = () => {
 
           <div className='field'>
               <label htmlFor="guests" className="">Number of Guests</label>
-              <input type="number" placeholder="guests"  name='guests'/>
-              <span>Error</span>
+              <input type="number" placeholder="guests"  name='guests' {...register("guests")} min="1" max="10"/>
+              <span>{errors.guests?.message}</span>
           </div>
 
           <div className='field'>
               <label htmlFor="date" className="htmlfor">Date & Time</label>
-              <input type="datetime-local" name="date" />
-              <span>Error</span>
+              <input type="datetime-local" name="date" {...register("date")}/>
+              <span>{errors.date?.message}</span>
           </div>
-          <button className='btn btn-primary'>Reserve</button>
+          <button className='btn' type="submit">Reserve</button>
         </fieldset>
     </form>
   )
--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -7,7 +7,7 @@ const BookingPage = () => {
     <main className='res-container'>
       <div className="container">
         <div className="res-content">
-            <div className="">
+            <div className="res-intro">
                 <h2 className='title'>Experience the perfect balance of tradition and luxury.</h2>
                 <p>At Little Lemon, we take great pride in providing our customers with the greatest luxurious experience with a touch of <em>tradition</em>.</p>
                 <p>Book a table with us to share in this experience.</p>
