b67bb085c238c75b34dd156f06ee93b8566eba2e
Olayande Samuel <samuelolayande@gmail.com>
2023-08-29 04:38:00 +0100

    Working on th reservation PAGE

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,9 +1,52 @@
 import React from 'react'
 
 const BookingForm = () => {
+
+
+
+
   return (
     <form>
-        
+        <fieldset>
+          <div className="field">
+            <label htmlFor="name">Full Name</label>
+            <input type="text" placeholder="Joe Biden" name="name"/>
+            <span>Errors</span>
+          </div>
+          <div className="field">
+            <label htmlFor="email">Email Address</label>
+            <input type="email" placeholder="joebiden@email.com" name="email"/>
+            <span>Errors</span>
+          </div>
+          <div className="field">
+            <label htmlFor="telephone">Telephone Number</label>
+            <input type="tel" placeholder="(123) 456-7890" name="telephone"/>
+            <span>Errors</span>
+          </div>
+
+          <div className='field'>
+              <label htmlFor="occasion" className="htmlfor">Occasion</label>
+              <select name="occasion" className="name">
+                <option value="select">Select Occasion</option>
+                <option value="birthday">Birthday</option>
+                <option value="engagement">Engagement</option>
+                <option value="anniversary">Anniversary</option>
+              </select>
+          </div>
+
+          <div className='field'>
+              <label htmlFor="guests" className="">Number of Guests</label>
+              <input type="number" placeholder="guests"  name='guests'/>
+              <span>Error</span>
+          </div>
+
+          <div className='field'>
+              <label htmlFor="date" className="htmlfor">Date & Time</label>
+              <input type="datetime-local" name="date" />
+              <span>Error</span>
+          </div>
+          <button className='btn btn-primary'>Reserve</button>
+        </fieldset>
     </form>
   )
 }
--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -5,7 +5,8 @@ import "./bookingpage.css";
 const BookingPage = () => {
   return (
     <main className='res-container'>
-      <div className="res-content">
+      <div className="container">
+        <div className="res-content">
             <div className="">
                 <h2 className='title'>Experience the perfect balance of tradition and luxury.</h2>
                 <p>At Little Lemon, we take great pride in providing our customers with the greatest luxurious experience with a touch of <em>tradition</em>.</p>
@@ -16,7 +17,9 @@ const BookingPage = () => {
                 <p>Please fill in and submit form to book your reservation at Little Lemon.</p>
                 <BookingForm />
             </div>
+        </div>
       </div>
+     
     </main>
   )
 }
